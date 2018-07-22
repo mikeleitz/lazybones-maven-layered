c9d80bb691a7387c89efc6d47fcd0ddfe3ff828c
Mike Leitz <mike@mikeleitz.com>
2018-07-22 00:12:38 -0500

    Update datasource and repository to JPA.

--- templates/maven-layered/database-jpa/src/main/java/AppUserRepository.java
+++ templates/maven-layered/database-jpa/src/main/java/AppUserRepository.java
@@ -1,11 +1,11 @@
 package ${base_package}.dao.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ${base_package}.dao.domain.AppUserDomain;
 
 @Repository
-public interface AppUserRepository extends CrudRepository<AppUserDomain, Long> {
+public interface AppUserRepository extends JpaRepository<AppUserDomain, Long> {
 
 }
