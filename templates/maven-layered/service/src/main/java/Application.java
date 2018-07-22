3601c8afb8255807b7bac02de4f1a97dc480c04a
Mike Leitz <mike@mikeleitz.com>
2018-07-21 19:01:52 -0500

    Revert "Add some persistence."
    This reverts commit 2ed7862

--- templates/maven-layered/service/src/main/java/Application.java
+++ templates/maven-layered/service/src/main/java/Application.java
@@ -2,28 +2,16 @@ package ${base_package}.service;
 
 import java.util.Arrays;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Import;
-
-import ${base_package}.dao.repository.AppUserRepository;
-import ${base_package}.dao.domain.AppUserDomain;
-import ${base_package}.config.${project_class_name}PersistenceConfig;
 
 @ComponentScan({"${base_package}"})
-@EntityScan("${base_package}.dao.domain")
-@Import(value = {${project_class_name}PersistenceConfig.class})
 @SpringBootApplication
 public class ${project_class_name}Application {
-    private static final Logger _logger = LoggerFactory.getLogger(${project_class_name}Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(${project_class_name}Application.class, args);
@@ -40,23 +28,8 @@ public class ${project_class_name}Application {
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
+
         };
     }
 
-    @Bean
-    public CommandLineRunner demo(AppUserRepository repository) {
-        return (args) -> {
-            repository.save(new AppUserDomain(null, "hello 1"));
-            repository.save(new AppUserDomain(null, "hello 2"));
-
-            _logger.info("AppUsers found with findAll():");
-            _logger.info("-------------------------------");
-
-            for (AppUserDomain user : repository.findAll()) {
-                _logger.info(user.toString());
-            }
-
-            _logger.info("");
-        };
-    }
 }
